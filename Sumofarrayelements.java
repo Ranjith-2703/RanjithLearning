5d7ec34e8905e721a9204d5eb83d8196f87e7dc3
Ranjith-2703 <ranjithkanna2703@gmail.com>
2025-03-18 17:23:03 +0530

    Create Sumofarrayelements.java

---
+++ Sumofarrayelements.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        int n=5,sum=0;
+        int arr[]  = {3,4,5,6,7};
+        for(int i=0;i<n;i++){
+            sum=sum+arr[i];
+        }
+        System.out.println(sum);
+    }
+}
